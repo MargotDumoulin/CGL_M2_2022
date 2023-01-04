4d7d2a0afcd015d095f69fe2a6be4a6b7f09002f
darkshaadow <darkshaadowdev@gmail.com>
2023-01-04 13:35:04 +0100

    Ajout lombok

--- src/main/java/fr/ul/projectcgl_dumoulin_duluye_schaffhauser/cgl_m2_2022/Model/Affaire.java
+++ src/main/java/fr/ul/projectcgl_dumoulin_duluye_schaffhauser/cgl_m2_2022/Model/Affaire.java
@@ -1,10 +1,18 @@
 package fr.ul.projectcgl_dumoulin_duluye_schaffhauser.cgl_m2_2022.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Affaire {
     private int id;
     private Apporteur apporteur;
@@ -14,59 +22,11 @@ public class Affaire {
     // Liste avec id de l'apporteur et montant de la commission associée 
     private Map<Integer, Double> commissionsPerso;
 
-    public Affaire(int id, Apporteur apporteur, Date date, float commission, Map<Integer, Double> commissionsPerso) {
-        this.id = id;
-        this.apporteur = apporteur;
-        this.date = date;
-        this.commission = commission;
-        this.commissionsPerso = commissionsPerso;
-    }
-
     public Affaire(int id, Apporteur apporteur, Date date, float commission) {
         this.id = id;
         this.apporteur = apporteur;
         this.date = date;
         this.commission = commission;
-        this.commissionsPerso = new HashMap<Integer, Double>();
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public Apporteur getApporteur() {
-        return this.apporteur;
-    }
-
-    public void setApporteur(Apporteur apporteur) {
-        this.apporteur = apporteur;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public float getCommission() {
-        return commission;
-    }
-
-    public void setCommission(float commission) {
-        this.commission = commission;
-    }
-
-    public Map<Integer, Double> getCommissionsPerso() {
-        return commissionsPerso;
-    }
-
-    public void setCommissionsPerso(Map<Integer, Double> commissionsPerso) {
-        this.commissionsPerso = commissionsPerso;
+        this.commissionsPerso = new HashMap<>();
     }
 }
--- src/main/java/fr/ul/projectcgl_dumoulin_duluye_schaffhauser/cgl_m2_2022/Model/Apporteur.java
+++ src/main/java/fr/ul/projectcgl_dumoulin_duluye_schaffhauser/cgl_m2_2022/Model/Apporteur.java
@@ -1,8 +1,17 @@
 package fr.ul.projectcgl_dumoulin_duluye_schaffhauser.cgl_m2_2022.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Apporteur {
     private int id;
     private boolean affilie;
@@ -15,57 +24,6 @@ public class Apporteur {
         this.affilie = affilie;
         this.nom = nom;
         this.prenom = prenom;
-        this.parrains = new ArrayList<Apporteur>();
+        this.parrains = new ArrayList<>();
     }
-
-    public Apporteur(int id, boolean affilie, String nom, String prenom, List<Apporteur> parrains) {
-        this.id = id;
-        this.affilie = affilie;
-        this.nom = nom;
-        this.prenom = prenom;
-        this.parrains = parrains;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public boolean isAffilie() {
-        return affilie;
-    }
-
-    public void setAffilie(boolean affilie) {
-        this.affilie = affilie;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getPrenom() {
-        return prenom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
-
-    public List<Apporteur> getParrains() {
-        return parrains;
-    }
-
-    public void setParrains(List<Apporteur> parrains) {
-        this.parrains = parrains;
-    }
-
-
-
 }
