844abb607a4eeac5b277d42b06c789f3432dcd19
Snorloux <margot.dumoulin18@gmail.com>
2023-01-11 16:31:26 +0100

    :construction: WIP sort

--- src/main/java/fr/ul/projectcgl_dumoulin_duluye_schaffhauser/cgl_m2_2022/DAO/ApporteurDAO.java
+++ src/main/java/fr/ul/projectcgl_dumoulin_duluye_schaffhauser/cgl_m2_2022/DAO/ApporteurDAO.java
@@ -25,6 +25,32 @@ public class ApporteurDAO extends AbstractDAO<ApporteurEntity, Long> {
         super(ApporteurEntity.class);
     }
 
+    public Stream<ApporteurEntity> getAll(int pageSize, int start) {
+        LocalDate currentDate = LocalDate.now();
+        LocalDate m1Date = LocalDate.from(currentDate).minusMonths(1);
+        LocalDate m2Date = LocalDate.from(currentDate).minusMonths(2);
+
+        String sqlQuery = """
+                SELECT t_apporteur.*
+                FROM apporteur AS t_apporteur
+                LEFT JOIN (SELECT t_commission.*
+                           FROM commission t_commission
+                           LEFT JOIN affaire AS t_affaire ON t_commission.AFFAIRE_ID = t_affaire.ID
+                           WHERE YEAR(t_affaire.DATE) = :month
+                             AND MONTH(t_affaire.DATE) = :year) AS c ON c.APPORTEUR_ID = t_apporteur.ID
+                GROUP BY t_apporteur.id
+                ORDER BY COALESCE(SUM(c.MONTANT), 0) ASC
+                """;
+
+        return getSession()
+                .createNativeQuery(sqlQuery, ApporteurEntity.class)
+                .setFirstResult(start)
+                .setMaxResults(pageSize)
+                .setParameter("month", currentDate.getMonthValue())
+                .setParameter("year", currentDate.getYear())
+                .getResultStream();
+    }
+
     public Boolean getIsAffilie(Long apporteurId) {
         LocalDate compDate = LocalDate.from(LocalDate.now()).minusMonths(3);
         String hqlQuery = "" +
